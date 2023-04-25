b4f877758fd195baf85f5733944d44eba80921e4
Roodabe <rashoorpouri@gmail.com>
2023-04-25 22:02:39 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
+
+let i = 0;
+
+Array.from(document.querySelector(".nav-link")).forEach((item) => {
+  item.style.cssText = `background-color: ${colors[i++]}`;
+});
+
+Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
+  item.onclick = () => {
+    item.parentElement.parentElement.classList.toggle("change");
+  };
+});
