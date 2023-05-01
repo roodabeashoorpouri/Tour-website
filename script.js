f7ad41d9904595356ec5519e255a1f8eb8fc89b2
Roodabe <rashoorpouri@gmail.com>
2023-05-01 10:55:38 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+const container = document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");
+});
+
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+});
+
 const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
 
 let i = 0;
 
-Array.from(document.querySelector(".nav-link")).forEach((item) => {
+Array.from(document.querySelectorAll(".nav-link")).forEach((item) => {
   item.style.cssText = `background-color: ${colors[i++]}`;
 });
 
